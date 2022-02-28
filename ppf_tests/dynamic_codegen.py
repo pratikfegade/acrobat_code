ba7a578e3659bd8c84b3b7cb4f9fc188b0a4999c
Pratik Fegade <pratikfegade@gmail.com>
2022-02-28 09:51:22 -0500

    WIP

--- ppf_tests/dynamic_codegen.py
+++ ppf_tests/dynamic_codegen.py
@@ -24,35 +24,35 @@ def matmul_add(N, L, M, dtype):
 
 target = tvm.target.Target("llvm")
 N = L = M = 1024
-V = te.var('Vavavoom')
-N = V
+# V = te.var('Vavavoom')
+N =  7865
 # task = tvm.auto_scheduler.SearchTask(func=matmul_add, args=(N, L, M, "float32"), target=target)
 compute_dag = tvm.auto_scheduler.ComputeDAG(matmul_add(N, L, M, "float32"))
 task = tvm.auto_scheduler.SearchTask(func=matmul_add, args=(N, L, M, "float32"),
                                      compute_dag=compute_dag, target=target)
-ctask = task.make_concrete({V: 128})
-print(ctask.compute_dag)
-exit(0)
+# ctask = task.make_concrete({V: 7865})
+# print(ctask.compute_dag)
 # Inspect the computational graph
 print("Computational DAG:")
 print(task.compute_dag)
 
-log_file = "matmul.json"
+log_file = "/home/ppf/Documents/projects/projects/dyn_batch/tvm/build/matmul.json"
 tune_option = auto_scheduler.TuningOptions(
-    num_measure_trials=0,
+    num_measure_trials=1,
     measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
     verbose=1,
 )
 
-# task.tune(tune_option)
+task.tune(tune_option)
 
-####################################################
-# V = te.var('V')
-V = N*2
-task2 = tvm.auto_scheduler.SearchTask(func=matmul_add, args=(V, L*2, M*2, "float32"), target=target)
-####################################################
+# ####################################################
+# # V = te.var('V')
+# V = N*2
+# task2 = tvm.auto_scheduler.SearchTask(func=matmul_add, args=(V, L*2, M*2, "float32"), target=target)
+# ####################################################
 
-sch, args = task2.apply_best_from_other_task(log_file, task)
+# sch, args = task.apply_best_from_other_task(log_file, task)
+sch, args = task.apply_best(log_file)
 
 print("Lowered TIR:")
 print(tvm.lower(sch, args, simple_mode=True))
